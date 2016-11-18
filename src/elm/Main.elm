1f1597391d2e44aff31df410586e3f9f4228b01c
Piper Niehaus <piper.niehaus@gmail.com>
2016-11-18 11:26:04 -0700

    basic baord and guessing

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,62 +1,86 @@
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+module Main exposing (..)
 
--- component import example
-import Components.Hello exposing ( hello )
+import Html exposing (..)
+import Html.Attributes exposing (placeholder, value)
+import Html.Events exposing (onClick, onInput)
 
 
 -- APP
-main : Program Never Int Msg
+
+
+main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram { model = model, view = view, update = update }
+
 
 
 -- MODEL
-type alias Model = Int
 
-model : number
-model = 0
+
+type alias Model =
+    { board : Board, score : Int, currentGuess : String }
+
+
+type alias Board =
+    List (List String)
+
+
+model : Model
+model =
+    { board = board, score = 0, currentGuess = "" }
+
+
+board : List (List String)
+board =
+    [ [ "a", "b" ], [ "c", "d" ] ]
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+
+
+type Msg
+    = NoOp
+    | CheckWord
+    | UpdateGuess String
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        NoOp ->
+            model
+
+        CheckWord ->
+            { model
+                | score = model.score + (String.length model.currentGuess)
+                , currentGuess = ""
+            }
+
+        UpdateGuess guess ->
+            { model | currentGuess = guess }
+
 
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
-    ]
-  ]
+    let
+        makeRow row =
+            div [] (List.map makeLetter row)
 
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+        makeLetter letter =
+            span [] [ text letter ]
+    in
+        div []
+            [ h2 [] [ text <| toString model.score ]
+            , div [] (List.map makeRow model.board)
+            , div []
+                [ input [ placeholder "Guess away!", onInput UpdateGuess, value model.currentGuess ] []
+                , button [ onClick CheckWord ] [ text "Check" ]
+                ]
+            ]
